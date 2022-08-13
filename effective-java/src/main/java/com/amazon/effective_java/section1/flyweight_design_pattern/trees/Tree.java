8e0c646a7414193711e2ec7c2cedf1483ac28d8b
r.rahmani <r.rahmani@vasl.ir>
2022-08-13 13:11:35 +0430

    Add Tree Class

---
+++ effective-java/src/main/java/com/amazon/effective_java/section1/flyweight_design_pattern/trees/Tree.java
@@ -0,0 +1,12 @@
+package com.amazon.effective_java.section1.flyweight_design_pattern.trees;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Tree {
+
+    private int x;
+    private int y;
+}

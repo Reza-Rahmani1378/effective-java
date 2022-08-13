c3d42eb2b5a4fe52f552d7f71f0d120730b6b6c4
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-08-13 18:35:58 +0430

    Use Design Pattern Flywiehgt for minimizing memory intake.

---
+++ effective-java/src/main/java/com/amazon/effective_java/section1/flyweight_design_pattern/trees/Forest.java
@@ -0,0 +1,25 @@
+package com.amazon.effective_java.section1.flyweight_design_pattern.trees;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+
+    private List<Tree> trees = new ArrayList<>();
+
+
+    public void plantTree(int x, int y, String name, Color color, String otherData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+}

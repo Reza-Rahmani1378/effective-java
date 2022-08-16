31ff95f1a7c7151e06f051cddf22e9f4d430e8f4
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-08-16 16:09:35 +0430

    Add example for method peek

---
+++ effective-java/src/main/java/com/amazon/effective_java/section1/item6/adapter_design_pattern/round/RoundHole.java
@@ -0,0 +1,8 @@
+package com.amazon.effective_java.section1.item6.adapter_design_pattern.round;
+
+public class RoundHole {
+
+    private double radios;
+
+
+}
---
+++ effective-java/src/main/java/com/amazon/effective_java/section1/item6/adapter_design_pattern/round/RoundPeg.java
@@ -0,0 +1,4 @@
+package com.amazon.effective_java.section1.item6.adapter_design_pattern.round;
+
+public class RoundPeg {
+}

1be4446cfcd363963ca614dc0d5f2f8058bc5260
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-08-17 10:56:21 +0430

    Learn Design Pattern Adapter

--- effective-java/src/main/java/com/amazon/effective_java/section1/item6/adapter_design_pattern/round/RoundHole.java
+++ effective-java/src/main/java/com/amazon/effective_java/section1/item6/adapter_design_pattern/round/RoundHole.java
@@ -4,5 +4,17 @@ public class RoundHole {
 
     private double radios;
 
+    public RoundHole(double radios) {
+        this.radios = radios;
+    }
 
+
+    public double getRadios() {
+        return radios;
+    }
+
+
+    public boolean fits(RoundPeg peg) {
+        return this.radios >= peg.getRadios();
+    }
 }
--- effective-java/src/main/java/com/amazon/effective_java/section1/item6/adapter_design_pattern/round/RoundPeg.java
+++ effective-java/src/main/java/com/amazon/effective_java/section1/item6/adapter_design_pattern/round/RoundPeg.java
@@ -1,4 +1,19 @@
 package com.amazon.effective_java.section1.item6.adapter_design_pattern.round;
 
 public class RoundPeg {
+
+    private double radios;
+
+    public RoundPeg() {
+
+    }
+
+    public RoundPeg(double radios) {
+        this.radios = radios;
+    }
+
+    public double getRadios() {
+        return radios;
+    }
+
 }

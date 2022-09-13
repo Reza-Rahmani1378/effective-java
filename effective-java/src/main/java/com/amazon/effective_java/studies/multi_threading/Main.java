5696f6575a3f312fd857c64fff15e18d7498b9db
Reza <reza6273757@gmail.com>
2022-09-13 13:04:37 +0430

    Create Thread with Runable interface

---
+++ effective-java/src/main/java/com/amazon/effective_java/studies/multi_threading/Main.java
@@ -0,0 +1,23 @@
+package com.amazon.effective_java.studies.multi_threading;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread myThread = new Thread(() -> {
+            System.out.println("We are in thread " + Thread.currentThread().getName());
+            System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+            throw new RuntimeException("Internal Exception...");
+
+        });
+        System.out.println("We are in thread :" + Thread.currentThread().getName() + " before starting new thread");
+        myThread.setName("new worker thread.");
+        myThread.setPriority(Thread.MAX_PRIORITY);
+        myThread.start();
+        System.out.println("We are in thread :" + Thread.currentThread().getName() + " after starting new thread");
+        myThread.setUncaughtExceptionHandler((thread, throwable) -> {
+
+            System.out.println("A critical exception happened in thread " + thread.getName() + " the error is " + throwable.getMessage());
+
+        });
+
+    }
+}

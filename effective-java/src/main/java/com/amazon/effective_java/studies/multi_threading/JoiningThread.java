269e0e608baf938cf560092454f12bae04c3a596
Reza <reza6273757@gmail.com>
2022-09-14 13:31:37 +0430

    Update Factorial

--- effective-java/src/main/java/com/amazon/effective_java/studies/multi_threading/JoiningThread.java
+++ effective-java/src/main/java/com/amazon/effective_java/studies/multi_threading/JoiningThread.java
@@ -1,5 +1,10 @@
 package com.amazon.effective_java.studies.multi_threading;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JoiningThread {
 
     /**
@@ -7,6 +12,50 @@ public class JoiningThread {
      * @param args
      */
     public static void main(String[] args) {
+        List<Long> inputNumbers = Arrays.asList(0L , 23L, 34L, 23442L , 234L);
+        List<FactorialThread> threads = new ArrayList<>();
+        for (Long inputNumber : inputNumbers) {
+            threads.add(new FactorialThread(inputNumber));
+        }
 
+        for (FactorialThread thread : threads) {
+            thread.start();
+        }
+
+
+    }
+
+
+    private static class FactorialThread extends Thread {
+        private long inputNumber;
+        private BigInteger result = BigInteger.ZERO;
+        private boolean isFinish;
+        public FactorialThread(long inputNumber) {
+            this.inputNumber = inputNumber;
+        }
+
+        @Override
+        public void run() {
+            this.result = factorial(inputNumber);
+            this.isFinish = true;
+        }
+
+        public BigInteger factorial(long inputNumber) {
+            BigInteger tempResult = BigInteger.ONE;
+            for (long i = inputNumber; i > 0 ; i --) {
+                tempResult = tempResult.multiply(new BigInteger(Long.toString(i)));
+            }
+
+            return tempResult;
+        }
+
+
+        public BigInteger getResult() {
+            return result;
+        }
+
+        public boolean isFinish() {
+            return isFinish;
+        }
     }
 }

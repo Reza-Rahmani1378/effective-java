0bf4d0b4ed70d6af6bac5ee5f57cc6661ae369b5
Reza <reza6273757@gmail.com>
2022-09-14 12:43:09 +0430

    Calculating Factorial with Joining Thread.

---
+++ effective-java/src/main/java/com/amazon/effective_java/studies/multi_threading/JoiningThread.java
@@ -0,0 +1,12 @@
+package com.amazon.effective_java.studies.multi_threading;
+
+public class JoiningThread {
+
+    /**
+     * Calculating Factorial with Joining Thread.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
